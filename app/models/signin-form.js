1cad153106e35645fd7feab0b156b3ceec471c92
rahul <rahulrajj6580@gmail.com>
2024-05-15 17:05:33 +0530

    First review

--- app/models/signin-form.js
+++
@@ -1,6 +0,0 @@
-import Model, { attr } from '@ember-data/model';
-
-export default class SigninFormModel extends Model {
-  @attr('string') emailId;
-  @attr('string') password;
-}
--- app/router.js
+++ app/router.js
@@ -9,7 +9,4 @@ export default class Router extends EmberRouter {
 Router.map(function () {
   this.route('login', { path: '/' });
   this.route('dashboard');
-  this.route('test');
-  this.route('test-chart');
-  this.route('carbon-calc');
 });
