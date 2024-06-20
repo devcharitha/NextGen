29e53246be4ebd7d9e66f23c65a0d7fdade8ba95
rahul <rahulrajj6580@gmail.com>
2024-06-20 15:43:06 +0530

    added select account page

---
+++ app/components/reward.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+
+export default class RewardComponent extends Component {
+  activity = {
+      activityCompletionDate: '2024-05-31',
+      activityName: 'Quiz',
+      activityStartDate: '2024-05-21',
+      pointsEarned: '50',
+      rewardsEarned: '5',
+    }
+  
+}
