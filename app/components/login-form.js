989290f9a06b029f90c9b8973e0fd35e19c69631
devcharitha <charithadasari05@gamil.com>
2024-07-05 17:35:07 +0530

    worked in authorization api

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
-// import * as jwt_decode from 'jwt-decode';
+
 export default class LoginFormComponent extends Component {
   @service router;
   @service token;
@@ -17,10 +17,17 @@ export default class LoginFormComponent extends Component {
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     return emailPattern.test(email);
   }
-  
+
+  validatePassword(password) {
+    const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,12}$/;
+    return passwordPattern.test(password);
+  }
+
   @action
   async signIn(event) {
     event.preventDefault();
+
+    this.errorMessage = '';
 
     if (!this.email && !this.password) {
       this.errorMessage = 'Enter the email and password';
@@ -28,8 +35,7 @@ export default class LoginFormComponent extends Component {
         this.errorMessage = '';
       }, 3000);
     } else if (!this.email) {
-      this.emailError = 'Enter an email ';
-
+      this.emailError = 'Enter the email ';
       setTimeout(() => {
         this.emailError = '';
       }, 3000);
@@ -43,20 +49,19 @@ export default class LoginFormComponent extends Component {
       setTimeout(() => {
         this.passwordError = '';
       }, 3000);
-    } else if (this.password.length < 6) {
-      this.passwordError = 'Invalid password';
+    } else if (!this.validatePassword(this.password)) {
+      this.passwordError = 'Invalid password format';
       setTimeout(() => {
         this.passwordError = '';
       }, 3000);
     } else {
       this.authenticateUser(this.email, this.password);
-      
     }
   }
   async authenticateUser(email, password) {
     // const api =
     //   'https://0t71wagdzi.execute-api.us-west-2.amazonaws.com/epic/authorization';
-    // try {
+    try {
     //   const response = await fetch(api, {
     //     method: 'POST',
     //     headers: {
@@ -64,26 +69,34 @@ export default class LoginFormComponent extends Component {
     //     },
     //     body: JSON.stringify({ email, password }),
     //   });
-    //   if (!response.ok) {
-    //     throw new Error('response is not ok', response);
-    //   }
-    //   const data = await response.json();
-    //   console.log('success', data);
 
-      // const decodedToken = jwt_decode.default(data.token);
-      // console.log('Decoded token:',decodedToken);
+    let response = this.store.createRecord('authorization', {
+      email: this.email,
+      password: this.password
+    });
+    response.save();
+    // if (!response) {
+    //   this.errorMessage = 'Invalid credentials';
+    //   setTimeout(() => {
+    //     this.errorMessage = '';
+    //   }, 3000);
+    //   return;
+    // }else{
+    //   console.log("authorization-response",response)
+    // }
 
-      let response=this.store.createRecord('authorization',{email,password});
-      response.save();
+    const tokenParts = response.access_token.split('.');
+    const decodedToken = JSON.parse(atob(tokenParts[1]));
+    console.log('Decoded token:', decodedToken);
 
-      const tokenParts = response.access_token.split('.');
-      const decodedToken = JSON.parse(atob(tokenParts[1]));
-      console.log('Decoded token:', decodedToken);
-
-      this.token.setToken(response.access_token, decodedToken);
-      this.router.transitionTo('accounts');
-    } catch (error) {
-      console.log('error', error);
+    this.token.setToken(response.access_token, decodedToken);
+    this.router.transitionTo('accounts');
+  } catch (error) {
+    console.log('error', error);
+    if (error.message.includes('Unauthorized')) {
+      this.errorMessage = 'Invalid credentials';
     }
   }
-// }
+  
+}
+}
--- app/models/authorization.js
+++ app/models/authorization.js
@@ -3,7 +3,5 @@ import { attr } from '@ember-data/model';
 
 export default class AuthorizationModel extends Model {
   @attr('string') email;
-  @attr('string') accountNumber;
-  @attr('string') customerId;
   @attr('string') password;
 }
